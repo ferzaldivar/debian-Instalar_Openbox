555019fa30c49bb280c5c4e680a0f234138b68a0
ferzaldivar <161621836+ferzaldivar@users.noreply.github.com>
2024-02-28 15:09:01 -0300

    Script

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#actualiza los repositorios concatenado con actualizar el sistema. el -y es para confirmar yes
+sudo apt update && sudo apt upgrade -y
+
+
+#para otorgarle permisos al archivo usar comando sudo chmod 777 script.sh
+#al listar los archivos en la terminal ahora es verde (ejecutable) antes era de color blanco
+
+
+
+
